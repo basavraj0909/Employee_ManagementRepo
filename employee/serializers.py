e87535615f51767fe8f72f6906838eddb76f36be
basavraj0909 <basavrajn123@gmail.com>
2024-09-01 20:06:51 +0530

    updated serializer file

--- employee/serializers.py
+++ employee/serializers.py
@@ -7,23 +7,12 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username','password',
-                  'first_name', 'last_name','email', 'phone_number']
+        fields = ['id', 'username', 'password',
+                  'first_name', 'last_name', 'email', 'phone_number']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
-
-    # user = CustomUser.objects.create_user(
-    #         username=validated_data['username'],
-    #         password=validated_data['password'],
-    #         first_name=validated_data.get('first_name', ''),
-    #         last_name=validated_data.get('last_name', ''),
-    #         email=validated_data['email'],
-    #         phone_number=validated_data['phone_number'],
-    #         date_of_birth=validated_data.get('date_of_birth', None)
-    #     )
-    #     return user
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -31,7 +20,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Employee
-        # fields = ['user', 'position', 'date_joined', 'updated_at']
         fields = ['user', 'position']
 
     def create(self, validated_data):
